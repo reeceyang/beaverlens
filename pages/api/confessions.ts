844af1b8d6a1f618f7e7e7a33994179901500597
Reece Yang <reeceyang@live.com>
2023-05-13 00:39:49 -0400

    initial commit

---
+++ pages/api/confessions.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../lib/mongodb";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("Cluster0");
+        const search_text = req.query.search_text;
+
+        const searcher_aggregate = {
+            $search: {
+                index: "search_confessions",
+                text: {
+                    query: search_text,
+                    path: {
+                        wildcard: "*",
+                    },
+                    fuzzy: {},
+                },
+            },
+        };
+        const projection = {
+            $project: {
+                post_text: 1,
+                time: 1,
+                post_url: 1,
+            },
+        };
+
+        const confessions = await db
+            .collection("confessions")
+            .aggregate([searcher_aggregate, projection])
+            .limit(10)
+            .toArray();
+
+        console.log(confessions);
+
+        res.json(confessions);
+    } catch (e) {
+        console.error(e);
+    }
+};
