cbedd3dabc6e76d58b9f8f1e2533ba1015a41b5f
Reece Yang <reeceyang@live.com>
2023-11-04 00:49:48 -0400

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,13 @@ import "../globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-
+import type { Metadata } from 'next'
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+
+export const metadata: Metadata = {
+  title: "Beaver Lens",
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +18,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="overflow-x-clip">
         <Header />
         {children}
